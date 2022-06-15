f05af7dca56eff1fa236722e78c810946df6bde1
Gaurav Rajput <gauravrajput2771@gmail.com>
2022-06-15 18:51:27 -0400

    virtual

---
+++ virtualenvironment.py
@@ -0,0 +1,6 @@
+#                                                     creating virtual environment
+# ye ham isliye bnante h jab hamra code purana ho jata h to kuch sal bad usme use hue module update k karan khtam hojate h use haamra code run nhi hoga kisi dusre ko dene par iske liye ham virtual environment ka use krte h isme ek file ham bnakr dete h usme sare version hote h jo hmne use kiye or unhe vo bnda command chalakar phle ke version me us code ko run kr skta h
+#
+# iske liye ek new folder bnane k bad use kholne k bad shift k sath right click krte h to window powershell ka option ata h use open krte h fir usme pip install krte h or  pip install virtualenv command ka use krte h  fir uske bad
+# virtualenv gauravvirtual ye command chalate h or gauravvirtual nam ki ek file bn jati h
+# .\gauravvirtual\Scripts\activate fir ise chalyenge
